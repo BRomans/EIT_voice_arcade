09bcd74b3c07e14af24a3b58aaed1db6ec6fb1ef
pablo <zackinator@gmail.com>
2019-09-25 14:02:06 +0200

    basic flappy bird

---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawn;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        columns = new GameObject[columnPoolSize];
+
+        for (int i=0; i<columnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (!GameController.instance.gameOver && timeSinceLastSpawn >= spawnRate)
+        {
+            timeSinceLastSpawn = 0;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void birdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void birdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
