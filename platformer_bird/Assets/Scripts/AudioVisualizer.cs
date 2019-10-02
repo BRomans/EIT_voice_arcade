f2c50ef216fb3fcd7bfe8135f98b5d03c48e53ff
pablo <zackinator@gmail.com>
2019-10-02 15:37:09 +0200

    initial commit for the platformer extension

---
+++ platformer_bird/Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioVisualizer : MonoBehaviour
+{
+    private float volume;
+    private float volumeLast;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        volume = GetComponent<MicrophoneInput>().GetAveragedVolume();
+
+        if (Mathf.Abs(volumeLast - volume) > .05f)
+        {
+            GameController.instance.birdFlap();
+        }
+
+        volumeLast = volume;
+    }
+}
---
+++ platformer_bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
+
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    public float upForce = 200f;
+    
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    public Text volumeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        setupActions();
+        setupRecognizer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        if (!isDead) {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, upForce));
+            anim.SetTrigger("Flap");
+        }
+    }
+
+    public void Die() {
+        if(!isDead) {
+            rb2d.velocity = Vector2.zero;
+            isDead = true;
+            anim.SetTrigger("Die");
+            GameController.instance.birdDied();
+        }
+    }
+
+    private void Restart() {
+        if(isDead) {
+            GameController.instance.restartGame();
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameController.instance.birdDied();
+    }
+
+    public bool isBirdDead()  {
+        return isDead;
+    }
+
+     // voice recognition implementation
+
+    private void setupActions() {
+        actions.Add("jump", () => {
+            Debug.Log("Bird is jumping");
+            Jump();
+        });
+        actions.Add("start", () => {
+            //TriggerJump()
+            Debug.Log("Game has started");
+            Restart();
+        });
+        
+        actions.Add("die", () => {
+            Debug.Log("You psycho!");
+            Die();
+        });
+
+    }
+
+    private void setupRecognizer() {
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) {
+        Debug.Log(speech.text);
+        System.Action action;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (actions.TryGetValue(speech.text, out action))
+        {
+            action.Invoke();
+            //keywordAction.Invoke();
+        }
+    }
+}
