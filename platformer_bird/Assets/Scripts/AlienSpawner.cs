0157962e6ffc84f06ba54abfccf7b508ba98f6c3
pablo <zackinator@gmail.com>
2019-10-16 16:31:41 +0200

    random alien spawning

--- platformer_bird/Assets/Scripts/AlienSpawner.cs
+++ platformer_bird/Assets/Scripts/AlienSpawner.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class AlienSpawner : MonoBehaviour
 {
-    public GameObject enemy;                // The enemy prefab to be spawned.
-    public float spawnTime = 5f;            // How long between each spawn.
+    public GameObject enemy;
+    public float spawnTime = 5f;
+
+    private float timeSinceLastSpawn;
+    private float spawnRate = 4f;
+    private float spawnRateMin = 5f;
+    private float spawnRateMax = 12f;
+    private float alienYsetPosistion = -1.6f;
+    private float randomXMin = 5f;
+    private float randomXMax = 12f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        //InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-    void Spawn()
-    {
-        // If the player has no health left...
-        if (GameController.instance.gameOver)
-        {
-            // ... exit the function.
-            return;
-        }
+    //void Spawn()
+    //{
+    //    if (GameController.instance.gameOver)
+    //    {
+    //        // ... exit the function.
+    //        return;
+    //    }
 
-        // Create an instance of the enemy prefab at the specified position.
-        Instantiate(enemy, new Vector3(10, -1.6f, 0), Quaternion.identity);
+    //    // Create an instance of the enemy prefab at the specified position.
+    //    Instantiate(enemy, new Vector3(10, alienYsetPosistion, 0), Quaternion.identity);
 
-    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // make spawn rate random
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+
+        if (!GameController.instance.gameOver && timeSinceLastSpawn >= spawnRate)
+        {
+            timeSinceLastSpawn = 0;
+
+            float spawnXPosition = Random.Range(randomXMin, randomXMax);
+
+            Instantiate(enemy, new Vector3(10, alienYsetPosistion, 0), Quaternion.identity);
+        }
     }
 }
