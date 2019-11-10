ae0e59771a7a286f7b937fc035d4a4d964c1afc1
pablo <zackinator@gmail.com>
2019-11-10 21:05:28 +0100

    alien destroyer and better destroyer script

---
+++ platformer_bird/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public string toBeDestroyed;
+
+    void Start() { }
+
+    void Update() { }
+
+    /* Destroy specific colliding objects with correct name */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == toBeDestroyed)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
