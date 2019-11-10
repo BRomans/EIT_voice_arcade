9bf6e1197dcde1e1104d2d9ce1278372ace5138a
bromans <michele.romani.gzl0@gmail.com>
2019-11-10 15:44:22 +0100

    - :bug: Fixed bug preventing the score to be updated

--- platformer_bird/Assets/Scripts/Column.cs
+++ platformer_bird/Assets/Scripts/Column.cs
@@ -6,7 +6,7 @@ public class Column : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Bird")
+        if (other.gameObject.tag == "Player")
           {
               GameController.instance.birdScored();
           }
