7571885cbb486881d9ea882508cde13f62dc1536
bromans <michele.romani.gzl0@gmail.com>
2019-11-13 23:11:43 +0100

    :recycle: added missing files

---
+++ platformer_bird/Assets/Scripts/Crater.cs
@@ -0,0 +1,25 @@
+  ﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Obstacle class */
+public class Crater : MonoBehaviour
+{
+    /* Tell controller the player scored when bird passes object */
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+          {
+              GameController.instance.birdScored(1); // worth 1 point
+          }
+    }
+
+    /* Destroy lasers on contact with obstacles */
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "laser_0(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
