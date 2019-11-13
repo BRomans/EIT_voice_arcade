7480c7c9acff70367628005a14d29b3e97393b9d
pablo <zackinator@gmail.com>
2019-11-13 13:04:56 +0100

    added mic sensitivity dropdown menu

--- platformer_bird/Assets/Scripts/GameController.cs
+++ platformer_bird/Assets/Scripts/GameController.cs
@@ -15,6 +15,7 @@ public class GameController : MonoBehaviour
     public float scrollSpeed = -1.5f;
     public Text scoreText;
 
+    public Dropdown micSensitivitySelector;
     public string volumeSensitivity = "med"; // or "low" or "high"
 
     public GameObject bird;
@@ -44,6 +45,7 @@ public class GameController : MonoBehaviour
 
         if (gameStarted)
         {
+            updateVolumeSensitivity();
             startText.SetActive(false);
             scoreText2.SetActive(true);
             bird.GetComponent<PlayerController>().isFlapping = flapping;
@@ -92,9 +94,9 @@ public class GameController : MonoBehaviour
     }
 
     /* Set the volume sensitivy - at the beginning of the game */
-    private void updateVolumeSensitivity(string input)
+    private void updateVolumeSensitivity()
     {
-        volumeSensitivity = input;
+        volumeSensitivity = micSensitivitySelector.options[micSensitivitySelector.value].text;
     }
 
     /* Setup all game over attributes when bird dies */
